4ef5ee0102998f4a1f9b1f8d2e9be9765a4f702d
Steven Baldasty <sbaldasty@gmail.com>
2024-11-22 14:21:56 -0500

    more source fields

--- src/main/kotlin/com/bitflippin/bedivere/ui/SourceDetail.kt
+++ src/main/kotlin/com/bitflippin/bedivere/ui/SourceDetail.kt
@@ -3,19 +3,27 @@ package com.bitflippin.bedivere.ui
 import com.bitflippin.bedivere.editor.EditorState
 import com.bitflippin.bedivere.model.Source
 import com.bitflippin.bedivere.swing.BoundTextField
+import com.bitflippin.bedivere.swing.Property
+import javax.swing.JLabel
 import javax.swing.JPanel
-import kotlin.reflect.KMutableProperty1
 
 class SourceDetail(
     private val editorState: EditorState,
     private val model: Source) : JPanel() {
 
     private val titleTextField = boundTextField(Source::title)
+    private val urlTextField = boundTextField(Source::url)
+    private val descriptionTextField = boundTextField(Source::description)
 
     init {
+        add(JLabel("Title:"))
         add(titleTextField)
+        add(JLabel("URL:"))
+        add(urlTextField)
+        add(JLabel("Description:"))
+        add(descriptionTextField)
     }
 
-    private fun boundTextField(property: KMutableProperty1<Source, String>) =
+    private fun boundTextField(property: Property<Source>) =
         BoundTextField(model, property, editorState.hub.sourceListeners)
 }
