e44450a35712dea0defc8fa9fee79092f630dd8f
Steven Baldasty <sbaldasty@gmail.com>
2024-12-16 20:27:28 -0500

    move extension methods

--- src/main/kotlin/com/bitflippin/bedivere/swing/bind/Binder.kt
+++ src/main/kotlin/com/bitflippin/bedivere/swing/bind/Binder.kt
@@ -1,6 +1,9 @@
 package com.bitflippin.bedivere.swing.bind
 
 import com.bitflippin.bedivere.editor.ChangeListener
+import javax.swing.JComboBox
+import javax.swing.JTextField
+import kotlin.reflect.KMutableProperty1
 
 interface Binder<U, M, L> {
     val ui: U
@@ -9,3 +12,14 @@ interface Binder<U, M, L> {
 
     fun release()
 }
+
+fun <U, M> Binder<U, M, M>.textFieldBinder(
+    textField: JTextField, property:
+    KMutableProperty1<M, String>
+) = TextFieldBinder(textField, model, listeners, property)
+
+fun <U, M, P> Binder<U, M, M>.comboBoxBinder(
+    items: List<P>,
+    comboBox: JComboBox<P>,
+    property: KMutableProperty1<M, P>
+) = ComboBoxBinder(comboBox, model, listeners, property, items)
